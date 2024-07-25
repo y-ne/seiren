9b89b430ae4f0f82f0a3c3dacdd67dc17f1f06d8
kuu <akumakumu@gmail.com>
2024-07-26 00:06:30 +0700

    fix nest gen

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
       logging: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
