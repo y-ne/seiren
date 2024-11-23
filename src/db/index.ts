e6328921b502fc846000135ac391aa80cf5baa2d
millimeter <akumakumu@gmail.com>
2024-11-23 07:00:39 +0700

    basic drizzle configuration and testing migrations

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+  ssl: true
+});
+
+const db = drizzle({ client: pool });
+
+const result = await db.execute('select 1');
