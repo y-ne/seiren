c115960302bbf407f12e411dd680c5ee09b60d68
millimeter <akumakumu@gmail.com>
2024-11-23 08:01:45 +0700

    hi

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import { drizzle } from "drizzle-orm/node-postgres";
-import pkg from 'pg';
-const { Pool } = pkg;
+import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!
